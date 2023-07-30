4056fe08eb4f5dc580f349e4171b6e169cb43e97
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-07-30 18:24:56 -0300

    opcao de mudança de porta adicionada

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('dotenv').config();
+
 const mainRoute = require('./routes/Main');
 
 const app = express();
@@ -7,6 +9,6 @@ app.set('view engine', 'pug');
 
 app.use('/', mainRoute);
 
-app.listen(3000, () =>{
-    console.log("Executando em http://localhost:3000/.");
+app.listen(process.env.PORT || 3000, () =>{
+    console.log(`Executando em http://localhost:${process.env.PORT || 3000}/.`);
 });
