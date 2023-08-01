ceb3edc9062c5324c4558586d5fe1bbf2f56ab32
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-08-01 18:49:55 -0300

    rota config e view adicionada

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 require('dotenv').config();
 
 const mainRoute = require('./routes/Main');
+const configRoute = require('./routes/Config');
 
 const app = express();
 
 app.set('view engine', 'pug');
 
 app.use('/', mainRoute);
+app.use('/config', configRoute);
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log(`Executando em http://localhost:${process.env.PORT || 3000}/.`);
